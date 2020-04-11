a3318e4b3f794dd1265f9e3d35a3bf5509814e7d
Jeremy <2500940+jeremypaavola@users.noreply.github.com>
2020-04-11 09:06:01 +0100

    ES6 and standards updates, see full description
    - Replaced `var` with `let` and `const` where appropriate
    - Removed `blinkieBlink` function
    - Updated notice styling with pastel tones
    - Created new `duplicateNoticeStyles()` function which is appended to `document.head`, and which makes use of template literals. This replaces the previous styles that were appended to `body`.
    - Updated `!=` loose equality checks with `!==` strict equality checks to avoid risk of type coercion.
    - Incremented version

--- zendesk-check-open-tickets.user.js
+++ zendesk-check-open-tickets.user.js
@@ -1,50 +1,58 @@
 // ==UserScript==
 // @name         Zendesk Check Open Tickets
 // @namespace    http://tampermonkey.net/
-// @version      0.98 rev. 230676
+// @version      0.98.1
 // @description  Checks if this user has OPEN/PENDING/ON-HOLD/NEW tickets and will display a notification at the top if they do
 // @updateURL    https://github.com/senff/Zendesk-Check-Open-Tickets/raw/master/zendesk-check-open-tickets.user.js
-// @author       Senff
+// @author       Senff, modified by Jeremy Paavola
 // @match        https://*.zdusercontent.com/*
 // @grant        none
 // ==/UserScript==
 
-var $ = window.jQuery;
+const $ = window.jQuery;
 
 function checkTickets() {
-    var nudgeNudge;
-    var winkWink = 0;
-    var goThruTix = setInterval(function() {
-        if($('body').hasClass('saynoMORE')) {
+    let nudgeNudge;
+    let winkWink = 0;
+    let goThruTix = setInterval(function () {
+        if ($('body').hasClass('saynoMORE')) {
             clearInterval(goThruTix);
         } else {
-            if($('.type-Zendesk_History').length) {
-                var mainURL = (window.location != window.parent.location)
-                ? document.referrer
-                : document.location.href;
-                var URLdivider = mainURL.split('/');
-                var thisTicket = URLdivider.pop();
-                var thisUser = $('.user__info .user__info-row:first-child .value').text();
-                $('#support__history-content .type-Zendesk_History').each(function(ticket) { // Looping through this user's list of tickets
-                    var ticketLine = $(this).find('.subject a').html();
-                    var ticketNoLong = ticketLine.substring(ticketLine.indexOf("#") + 1); // Remove first character
-                    var ticketNo = ticketNoLong.substring(0, ticketNoLong.indexOf(' - ')); // Remove everything after first space
-                    if(ticketNo!=thisTicket) {
+            if ($('.type-Zendesk_History').length) {
+                const mainURL = ( window.location !== window.parent.location )
+                    ? document.referrer
+                    : document.location.href;
+                const URLdivider = mainURL.split('/');
+                const thisTicket = URLdivider.pop();
+                const thisUser = $('.user__info .user__info-row:first-child .value').text();
+                $('#support__history-content .type-Zendesk_History').each(function (ticket) { // Looping through this user's list of tickets
+                    const ticketLine = $(this).find('.subject a').html();
+                    const ticketNoLong = ticketLine.substring(ticketLine.indexOf("#") + 1); // Remove first character
+                    const ticketNo = ticketNoLong.substring(0, ticketNoLong.indexOf(' - ')); // Remove everything after first space
+                    if ( ticketNo !== thisTicket ) {
                         // This entry is not the ticket we're looking at
-                        if(($(this).hasClass('status-Open')) || ($(this).hasClass('status-Pending')) || ($(this).hasClass('status-Hold')) || ($(this).hasClass('status-New'))) { // There's other tickets, aside from this one
+                        if (($(this).hasClass('status-Open')) || ($(this).hasClass('status-Pending')) || ($(this).hasClass('status-Hold')) || ($(this).hasClass('status-New'))) { // There's other tickets, aside from this one
                             winkWink = winkWink + 1;
                         }
                     } else {
                         $(this).addClass('this-ticket');
                     }
                 });
-                if(winkWink > 0) {
+                if (winkWink > 0) {
                     $('body').addClass('saynoMORE');
-                    nudgeNudge = "<div class='nudgeNudge'><span style='font-size:18px;'>⚠️</span> <span class='whatsItLike'><strong>THIS USER HAS OTHER OPEN/PENDING TICKETS!</strong> Please check those first before you reply here.</span>&nbsp;&nbsp;&nbsp;<span class='cool'>Cool, got it.</span></strong></div>";
+                    nudgeNudge = `
+                        <div class='nudgeNudge'>
+                            <span style='font-size:18px;'>⚠️</span>
+                                <span class='whatsItLike'>
+                                    <strong>This user has other/open pending tickets!</strong>
+                                    Please check those first before you reply here.
+                                </span>
+                                &nbsp;&nbsp;&nbsp;
+                                <span class='cool'>Cool, got it.</span>
+                        </div>`;
                     $(nudgeNudge).prependTo('body');
-                    var nudgeHeight = $('.nudgeNudge').outerHeight()+5;
-                    $('body').css('padding-top',nudgeHeight+'px');
-                    blinkieBlink();
+                    var nudgeHeight = $('.nudgeNudge').outerHeight() + 5;
+                    $('body').css('padding-top', nudgeHeight + 'px');
                 }
                 $('body').addClass('saynoMORE');
             }
@@ -53,19 +61,54 @@ function checkTickets() {
     }, 1000);
 }
 
-function blinkieBlink() {
-    setInterval(function(){
-    $('.nudgeNudge .whatsItLike').toggleClass('hidethis');
-  }, 800);
-}
-
 $('body').on('click','.nudgeNudge .cool',function(){
     $('.nudgeNudge .cool').html('Baiiiiiiiiii');
     $('.nudgeNudge').fadeOut(500);
     $('body').removeClass('saynoMORE').animate({padding: 0}, 800, function() {});
 });
 
+function duplicateNoticeStyles() {
+    const styles = document.createElement( 'style' );
+    styles.innerHTML = `
+    
+        .nudgeNudge { 
+            box-sizing: border-box;
+            position: fixed;
+            width: 100%;
+            top:0;
+            z-index: 100;
+            background: #D8456E;
+            color: #ffffff;
+            padding: 7px 10px 10px;
+            line-height: 20px;
+        }
+        .nudgeNudge .cool { 
+            color: #FBD45E; 
+            font-weight: bold;
+            text-decoration:underline;
+            cursor: pointer;
+        }
+        .this-ticket td { 
+            background: #e7e7e7 !important; 
+            filter: grayscale(100%);
+        }
+        .this-ticket td:first-child {
+            border-left: solid 1px #cccccc;
+        }
+        .this-ticket td.subject a,
+        .this-ticket td.when {
+            cursor: default;
+            pointer-events: none;
+            color: #888888 !important;
+            text-shadow: 1px 1px 0 #ffffff;
+            font-style: italic;
+            font-size: 12px;
+        }
+    `;
+    document.head.appendChild( styles );
+}
+
 $(document).ready(function() {
-    $('body').append('<style type="text/css" id="nudgestyles">.nudgeNudge{box-sizing:border-box; position: fixed; width: 100%; top:0; z-index: 100; background: #aa0000; color: #ffffff; padding: 7px 10px 10px; line-height: 20px;}.nudgeNudge .cool{color: #FFFFCC;font-weight: bold; text-decoration:underline; cursor: pointer;}.hidethis{opacity:0;}.this-ticket td{background: #e7e7e7 !important;filter: grayscale(100%);}.this-ticket td:first-child {border-left: solid 1px #cccccc;}.this-ticket td.subject a,.this-ticket td.when {cursor: default;pointer-events: none;color: #888888 !important;text-shadow: 1px 1px 0 #ffffff;font-style: italic;font-size: 12px;}</style>');
+    duplicateNoticeStyles();
     checkTickets();
 });
